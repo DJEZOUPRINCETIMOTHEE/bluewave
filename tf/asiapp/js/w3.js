0008519ddf9770f065c6700f8a57bc451b6ddda2
Djézou Prince Timothée <74551719+DJEZOUPRINCETIMOTHEE@users.noreply.github.com>
2023-12-11 16:36:15 +0000

    Update w3.js

--- tf/asiapp/js/w3.js
+++ tf/asiapp/js/w3.js
@@ -1,25 +1,25 @@
 // Get the modal
-var modal = document.getElementById('myModal');
+var w3modal = document.getElementById('w3myModal');
 
 // Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+var w3btn = document.getElementById("w3myBtn");
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+var w3span = document.getElementsByClassName("w3close")[0];
 
 // When the user clicks the button, open the modal 
-btn.onclick = function() {
-    modal.style.display = "block";
+w3btn.onclick = function() {
+    w3modal.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
+w3span.onclick = function() {
+    w3modal.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
+    if (event.target == w3modal) {
+        w3modal.style.display = "none";
     }
 }
